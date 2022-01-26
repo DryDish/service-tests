e4a95fd15cd0c10070c24ecd8ae6c3c349f7b141
IroniX2 <57399961+IroniX2@users.noreply.github.com>
2022-01-26 14:05:07 +0100

    feat(user-service): add basic test framework

--- apps/user_service/test/test.js
+++ apps/user_service/test/test.js
@@ -1,22 +1,5 @@
-// require('dotenv').config();
-// const http = require('http');
-// const qs = require('querystring');
-
-
-// const server = http.createServer(function(req, res) {
-//     console.dir(req.param);
-    
-// })
-
-
-// assert.response(server, {
-//     url: '/',
-//     method: 'GET'
-// }, {
-//     body: '{}',
-//     status: 200,
-//     headers: {
-//         'Content-Type': 'application/json; charset=utf8',
-//         'X-Foo': 'bar'        
-//     }
-// });
+describe('Sample Test', () => {
+    it('should test that true === true', () => {
+      expect(true).toBe(true)
+    })
+});
